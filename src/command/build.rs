360ce27aec0c1a30c770c23f4d76cd92e7d49d8e
Johnson He <hejo@iu.edu>
2025-10-10 22:20:02 -0400

    also need to unescape the < character

--- src/command/build.rs
+++ src/command/build.rs
@@ -146,6 +146,11 @@ fn symbol2forest(working_dir: &PathBuf, elem: &Element) -> String {
         } else {
             childtext.to_owned()
         };
+        let childtext = if childtext.contains("&lt;") {
+            childtext.replace("&lt;", "<")
+        } else {
+            childtext.to_owned()
+        };
         // TODO: Hack to get \startverb-- to work "properly" in forester
         let childtext = if childtext.starts_with("--") {
             childtext.replacen("--", " --", 1)
